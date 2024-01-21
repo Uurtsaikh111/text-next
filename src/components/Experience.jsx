3af17ab7c6fb0d4905734414ce00ed355cf23882
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-01-21 21:03:18 +0800

    Jan21

---
+++ src/components/Experience.jsx
@@ -0,0 +1,19 @@
+import { Upwork } from "./icons/Upwork";
+
+export function Experience(){
+    return(
+        <div className="container px-[80px] py-[96px] ">
+            <div className="flex felx-col gap-[48px] px-[32px] py-[0px] justify-center">
+                <div className="flex flex-col gap-[16px] items-center ">
+                    <h2 className="px-[20px] py-[4px] bg-gray-200 rounded-xl">Experience</h2>
+                    <p>Here is a quick summary of my most recent experiences:</p>
+                </div>
+                <div>
+                    
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
